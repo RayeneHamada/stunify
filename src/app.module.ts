841992868a2411baa9fee6cdf88a05ad8096063c
RayeneHamada <rayeeene@gmail.com>
2023-01-16 16:29:46 +0100

    Database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URL)
   ],
 })
 export class AppModule { }
